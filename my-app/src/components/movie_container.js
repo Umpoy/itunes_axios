8630dc03843bf0370625ac5cd82e9c69a504675d
Umpoy <iumpoy@yahoo.com>
2018-12-17 19:03:19 -0800

    mapped movie list

--- my-app/src/components/movie_container.js
+++ my-app/src/components/movie_container.js
@@ -6,21 +6,26 @@ class MoviesContainer extends Component {
     constructor(props) {
         super(props);
 
-        this.state = {}
+        this.state = {
+            movies: []
+        }
     }
 
     componentWillMount() {
         const url = 'http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topMovies/json';
         axios.get(url).then((resp) => {
             console.log('Resp:', resp.data.feed.entry);
+            this.setState({ movies: resp.data.feed.entry })
         });
     }
 
     render() {
+        const movieList = this.state.movies.map((movieInfo, index) => {
+            return <Movie info={movieInfo} key={index} />;
+        })
         return (
             <div>
-                <h2>Movie Container</h2>
-                <Movie />
+                {movieList}
             </div>
         )
     }
