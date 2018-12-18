33937a1f98f1e14cd73b789f031a67c1eb67be59
Umpoy <iumpoy@yahoo.com>
2018-12-17 20:14:54 -0800

    added video preview of movie

--- my-app/src/components/movie.js
+++ my-app/src/components/movie.js
@@ -8,12 +8,13 @@ const movieStyling = {
 
 export default props => {
     const { info } = props;
-    console.log('info: ', info);
+    console.log('info: ', info.link[1].attributes.href);
     return (
-        <div style={movieStyling}>
-            <h3>{info['im:name'].label}</h3>
+        <div style={movieStyling} className="text-center">
+            <h3 >{info['im:name'].label}</h3>
             <img src={info['im:image'][2].label} alt={info['im:name'].label} />
             <p>{info.summary.label}</p>
+            <a href={info.link[1].attributes.href} target="_blank">Preview</a>
         </div>
     );
 }
