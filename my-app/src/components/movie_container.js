aab279da94e786b54a990f14989df37aae346597
Umpoy <iumpoy@yahoo.com>
2018-12-17 17:54:56 -0800

    added constructor to movie_container

--- my-app/src/components/movie_container.js
+++ my-app/src/components/movie_container.js
@@ -3,6 +3,12 @@ import Movie from './movie';
 import axios from 'axios';
 
 class MoviesContainer extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {}
+    }
+
     componentWillMount() {
         const url = 'http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topMovies/json';
         axios.get(url).then((resp) => {
