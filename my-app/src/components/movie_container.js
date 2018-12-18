0cb5fe35d26c5f1ef8bcb85f24ade391948878c1
Umpoy <iumpoy@yahoo.com>
2018-12-17 17:49:36 -0800

    grabbed top movies from api

--- my-app/src/components/movie_container.js
+++ my-app/src/components/movie_container.js
@@ -1,7 +1,15 @@
 import React, { Component } from 'react';
 import Movie from './movie';
+import axios from 'axios';
 
 class MoviesContainer extends Component {
+    componentWillMount() {
+        const url = 'http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topMovies/json';
+        axios.get(url).then((resp) => {
+            console.log('Resp:', resp.data.feed.entry);
+        });
+    }
+
     render() {
         return (
             <div>
