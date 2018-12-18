4461816c7cf3460cd410ac3737035edbf027545a
Umpoy <iumpoy@yahoo.com>
2018-12-17 19:08:36 -0800

    movie title and images now render

--- my-app/src/components/movie.js
+++ my-app/src/components/movie.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
 export default props => {
+    const { info } = props;
+    console.log('info: ', info);
     return (
         <div>
-            <h3>Movie</h3>
+            <h3>{info['im:name'].label}</h3>
+            <img src={info['im:image'][2].label} alt={info['im:name'].label} />
         </div>
     );
 }
